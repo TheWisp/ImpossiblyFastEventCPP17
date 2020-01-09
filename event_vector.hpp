b2cccf8ce0edcaec7437ffa06d45f038eb758b6b
TheWisp <feiteng@gmail.com>
2020-01-09 17:37:40 +0100

    Small buffer optimization

--- event_vector.hpp
+++ event_vector.hpp
@@ -23,104 +23,173 @@ namespace ifevec
 			CallbackFuncType* func;
 		};
 
-		size_t m_Size = 0;
-		size_t m_Capacity = 0;
-		Callback* m_Callbacks = nullptr;
-		ListenerBase** m_Listeners = nullptr;
+		union
+		{
+			struct
+			{
+				Callback* m_Callbacks;
+				ListenerBase** m_Listeners;
+				size_t m_Size : 8 * sizeof size_t - 2;
+				size_t m_Calling : 1;//represents if we are in the middle of calling back, no matter recursive or not.
+				size_t m_Dirty : 1;
+			};
 
-		bool m_Calling = false; //represents if we are in the middle of calling back, no matter recursive or not. TODO use a single bit
-		bool m_Dirty = false; //TODO use a single bit
+			struct
+			{
+				Callback m_SingleCb;
+				ListenerBase* m_SingleListener;
+			};
+		};
+
+		size_t m_Capacity : 8 * sizeof size_t - 1;
+		size_t m_Multi : 1; //flag, 0 => holding single listener
 
 		void add(void* object, CallbackFuncType* func, ListenerBase* pListener)
 		{
-			if (m_Size == m_Capacity)
+			if (!m_Multi)
 			{
-				//grow and copy
-				size_t capacity = (m_Capacity * 3) / 2; //grow by 1.5
-				if (!capacity) capacity = 2;
-				Callback* callbacks = new Callback[capacity];
-				ListenerBase** listeners = new ListenerBase * [capacity];
-
-				for (size_t i = 0; i < m_Size; ++i)
+				if (!m_SingleListener)
 				{
-					callbacks[i] = m_Callbacks[i];
-					listeners[i] = m_Listeners[i];
+					m_SingleCb.object = object;
+					m_SingleCb.func = func;
+					m_SingleListener = pListener;
+					pListener->m_idx = 0;
 				}
-
-				m_Capacity = capacity;
-				delete[] m_Callbacks;
-				m_Callbacks = callbacks;
-				delete[] m_Listeners;
-				m_Listeners = listeners;
+				else
+				{
+					m_Multi = 1;
+					m_Capacity = 2;//initially 2 slots
+					Callback* callbacks = (Callback*)malloc(sizeof Callback * m_Capacity);
+					callbacks[0] = m_SingleCb;
+					callbacks[1].object = object;
+					callbacks[1].func = func;
+					ListenerBase** listeners = (ListenerBase **)malloc(sizeof(void*) * m_Capacity);
+					listeners[0] = m_SingleListener;
+					listeners[1] = pListener;
+					m_Callbacks = callbacks;
+					m_Listeners = listeners;
+					m_Size = 2;
+					m_Calling = 0;
+					m_Dirty = 0;
+					pListener->m_idx = 1;
+				}
 			}
-
-			m_Callbacks[m_Size].object = object;
-			m_Callbacks[m_Size].func = func;
-			m_Listeners[m_Size] = pListener;
-			pListener->m_idx = m_Size++;
+			else 
+			{
+				if (m_Size == m_Capacity)
+				{
+					//grow and copy
+					m_Capacity = (m_Capacity * 3) / 2; //grow by 1.5    2->3->4->6->9...
+					m_Callbacks = (Callback*)realloc(m_Callbacks, sizeof Callback * m_Capacity);
+					m_Listeners = (ListenerBase * *)realloc(m_Listeners, sizeof(void*) * m_Capacity);
+				}
+				m_Callbacks[m_Size].object = object;
+				m_Callbacks[m_Size].func = func;
+				m_Listeners[m_Size] = pListener;
+				pListener->m_idx = m_Size++;
+			}
 		}
 
 		void remove(size_t idx)
 		{
-			m_Callbacks[idx].object = m_Callbacks[idx].func = nullptr;
-			m_Listeners[idx] = nullptr;
-			m_Dirty = true;
+			if (m_Multi)
+			{
+				m_Callbacks[idx].object = m_Callbacks[idx].func = nullptr;
+				m_Listeners[idx] = nullptr;
+				m_Dirty = 1;
+			}
+			else
+			{
+				m_SingleCb.object = nullptr;
+				m_SingleCb.func = nullptr;
+				m_SingleListener = nullptr;
+			}
 		}
 
 		void replace(size_t idx, void* object, ListenerBase* listener)
 		{
-			m_Callbacks[idx].object = object;
-			m_Listeners[idx] = listener;
+			if (m_Multi)
+			{
+				m_Callbacks[idx].object = object;
+				m_Listeners[idx] = listener;
+			}
+			else
+			{
+				m_SingleCb.object = object;
+				m_SingleListener = listener;
+			}
 		}
 
 	public:
 
+		Event() : m_SingleCb(), m_SingleListener(), m_Capacity(), m_Multi()
+		{
+		}
+
 		~Event()
 		{
-			for (ListenerBase** l = m_Listeners, **end = m_Listeners + m_Size; l < end; ++l)
-				if (*l) (*l)->m_Event = nullptr;
-			delete[] m_Listeners;
-			delete[] m_Callbacks;
+			if (m_Multi)
+			{
+				for (ListenerBase** l = m_Listeners, **end = m_Listeners + m_Size; l < end; ++l)
+					if (*l) (*l)->m_Event = nullptr;
+				free(m_Listeners);
+				free(m_Callbacks);
+			}
+			else
+			{
+				if (m_SingleListener)
+					m_SingleListener->m_Event = nullptr;
+			}
 		}
 
 		template<typename ... ActualArgsT>
 		void operator()(ActualArgsT&& ... args)
 		{
-			bool isRecursion = m_Calling;
-			if (!m_Calling) m_Calling = true;
-
-			//TODO: using index is necessary because of potential reallocation during callback. Can we make it faster? (yet another flag?)
-			//TODO: alternatively, simply use another buffer for newly added ones during callback?
-			for (size_t i = 0; i < m_Size; ++i)
+			if (m_Multi)
 			{
-				auto& cb = m_Callbacks[i];
-				if (cb.func)
-					cb.func(cb.object, std::forward<ActualArgsT>(args)...);
-			}
+				bool isRecursion = m_Calling;
+				if (!m_Calling) m_Calling = 1;
 
-			if (!isRecursion)
-			{
-				m_Calling = false;
-
-				if (m_Dirty)
+				//TODO: using index is necessary because of potential reallocation during callback. Can we make it faster? (yet another flag?)
+				//TODO: alternatively, simply use another buffer for newly added ones during callback?
+				for (size_t i = 0; i < m_Size; ++i)
 				{
-					m_Dirty = false;
-					//remove all empty slots while patching the stored index in the listener
-					size_t sz = 0;
-					for (size_t i = 0; i < m_Size; ++i)
-					{
-						if (m_Listeners[i]) {
-							m_Listeners[sz] = m_Listeners[i];
-							m_Callbacks[sz] = m_Callbacks[i];
-							m_Listeners[sz]->m_idx = sz;
-							++sz;
-						}
-					}
-					m_Size = sz;
+					auto& cb = m_Callbacks[i];
+					if (cb.func)
+						cb.func(cb.object, std::forward<ActualArgsT>(args)...);
 				}
+
+				if (!isRecursion)
+				{
+					m_Calling = 0;
+
+					if (m_Dirty)
+					{
+						m_Dirty = 0;
+						//remove all empty slots while patching the stored index in the listener
+						size_t sz = 0;
+						for (size_t i = 0; i < m_Size; ++i)
+						{
+							if (m_Listeners[i]) {
+								m_Listeners[sz] = m_Listeners[i];
+								m_Callbacks[sz] = m_Callbacks[i];
+								m_Listeners[sz]->m_idx = sz;
+								++sz;
+							}
+						}
+						m_Size = sz;
+					}
+				}
+			}
+			else
+			{
+				if (m_SingleCb.func)
+					m_SingleCb.func(m_SingleCb.object, std::forward<ActualArgsT>(args)...);
 			}
 		}
 	};
+
+	static_assert(sizeof Event<void()> == 4 * sizeof size_t);
 
 	template<auto EventPtr, auto CallbackPtr> struct Listener;
 
